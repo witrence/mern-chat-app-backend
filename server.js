dfa152438f61cb930e9402ddd5c88a620224a7e8
witrence <48147336+witrence@users.noreply.github.com>
2023-05-20 12:53:48 +0530

    One more try Cors server.js

--- server.js
+++ server.js
@@ -21,11 +21,11 @@ const app = express();
 app.use(express.json());
 
 // enabling cors
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // moved to below deployement in else part
 app.get("/", (request, response) => {
